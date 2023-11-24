55add0d74c1bab205bab285f9e1d5a860caee59f
Arthur Luz <arthurluz04@hotmail.com>
2023-11-23 21:08:48 -0300

    8-directional movement
    Scripts and animations working

---
+++ The Frog Unity Files/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    [SerializeField ] private float moveSpeed;
+    private float ySpeed = 0.75f;
+    private float xSpeed = 1.0f;
+     private Vector2 moveDelta;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    
+    void FixedUpdate()
+    {
+        MoveCharacter(); 
+    }
+
+     void MoveCharacter()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput  = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector2(xInput * xSpeed, yInput * ySpeed);
+        moveDelta.Normalize();
+
+        // move the RigidBody2D instead of moving the Transform
+        rb.velocity = moveDelta * moveSpeed;
+
+        // animation direction
+        animator.SetFloat("XInput", moveDelta.x);
+        animator.SetFloat("YInput", moveDelta.y);
+        animator.SetFloat("Magnitude", moveDelta.magnitude);
+    }
+}
