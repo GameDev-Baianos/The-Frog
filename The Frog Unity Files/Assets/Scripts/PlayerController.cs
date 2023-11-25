641a3315fc51e9644851b4d2cb1bd728cad47e76
Arthur Luz <arthurluz04@hotmail.com>
2023-11-24 23:32:11 -0300

    Rolling animation
    add impulse

--- The Frog Unity Files/Assets/Scripts/PlayerController.cs
+++ The Frog Unity Files/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private float xSpeed = 1.0f;
      private Vector2 moveDelta;
 
+     private bool IsRolling = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,10 +24,19 @@ public class PlayerController : MonoBehaviour
         MoveCharacter(); 
     }
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsRolling)
+            animator.SetTrigger("IsRolling");
+    }
+
      void MoveCharacter()
     {
+        // Movement Input
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput  = Input.GetAxisRaw("Vertical");
+
+
 
         moveDelta = new Vector2(xInput * xSpeed, yInput * ySpeed);
         moveDelta.Normalize();
@@ -37,5 +48,6 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("XInput", moveDelta.x);
         animator.SetFloat("YInput", moveDelta.y);
         animator.SetFloat("Magnitude", moveDelta.magnitude);
+        
     }
 }
