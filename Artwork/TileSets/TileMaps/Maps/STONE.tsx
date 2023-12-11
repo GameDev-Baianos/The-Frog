6121216ced4adb4dc8ce14b1dc7ed87413dafd2e
Victor Calidor <victor.calidor@gmail.com>
2023-12-11 19:58:19 -0300

    Improving the map example

--- Artwork/TileSets/TileMaps/Maps/STONE.tsx
+++ Artwork/TileSets/TileMaps/Maps/STONE.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="STONE" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="../The frog/TileSets/TileMaps/Stone.png" width="320" height="320"/>
+ <image source="../Stone.png" width="320" height="320"/>
 </tileset>
