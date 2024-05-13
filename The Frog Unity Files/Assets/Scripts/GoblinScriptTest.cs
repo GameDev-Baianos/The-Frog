df1a45a6245f49997ace9a08705d8ead7ee14990
Arthur Luz <arthurluz04@hotmail.com>
2024-05-13 07:17:41 -0300

    GoblinScript update

--- The Frog Unity Files/Assets/Scripts/GoblinScriptTest.cs
+++ The Frog Unity Files/Assets/Scripts/GoblinScriptTest.cs
@@ -18,8 +18,8 @@ public class GoblinScriptTest : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //direction.Normalize();
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         // animation direction
         if(direction.x != 0 || direction.y != 0)
@@ -30,7 +30,7 @@ public class GoblinScriptTest : MonoBehaviour
         if(distance < 10)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            //transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
 }
